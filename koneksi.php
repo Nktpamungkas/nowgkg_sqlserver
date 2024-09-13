e36a5ec253fa636881e51dbe6373198ec1fd8e6e
hadi.subhana <hadi.subhana@indotaichen.com>
2024-09-13 14:29:59 +0700

    initial setting koneksi sql server

--- koneksi.php
+++ koneksi.php
@@ -22,12 +22,31 @@ else{
     exit("DB2 Connection failed");
     }
 
-$con1=mysqli_connect("10.0.0.10","dit","4dm1n","dbknitt");
-//$cond=mysqli_connect("10.0.0.10","dit","4dm1n","db_qc");
-$con=mysqli_connect("10.0.0.10","dit","4dm1n","dbnow_gkg");
-if (mysqli_connect_errno()) {
-printf("Connect failed: %s\n", mysqli_connect_error());
-exit();
-} 
+// $con1=mysqli_connect("10.0.0.10","dit","4dm1n","dbknitt");
+// //$cond=mysqli_connect("10.0.0.10","dit","4dm1n","db_qc");
+// $con=mysqli_connect("10.0.0.10","dit","4dm1n","dbnow_gkg");
+// if (mysqli_connect_errno()) {
+// printf("Connect failed: %s\n", mysqli_connect_error());
+// exit();
+// } 
+
+
+$hostSVR19 = "10.0.0.221";
+$usernameSVR19 = "sa";
+$passwordSVR19 = "Ind@taichen2024";
+
+// dbknitt belum ada jadi dimatikan koneksinya :)
+// $dbknitt = array("Database" => "dbknitt", "UID" => $usernameSVR19, "PWD" => $passwordSVR19);
+$dbnow_gkg = array("Database" => "dbnow_gkg", "UID" => $usernameSVR19, "PWD" => $passwordSVR19);
+
+// $con1 = sqlsrv_connect($hostSVR19, $dbknitt);
+$con = sqlsrv_connect($hostSVR19, $dbnow_gkg);
+
+// if (!$con1) {
+//     exit("SQLSVR19 Connection failed con1");
+// }
+if (!$con) {
+    exit("SQLSVR19 Connection failed con");
+}
 
 ?>

f163632902d8539f5f0c32cb465c3a196314b109
Muhammad Tio Laksono <33713735+ujangproject@users.noreply.github.com>
2025-01-08 16:43:11 +0700

    UP CEK SURAT JALAN MASUK KNT

--- pages/CekSuratJalanMasuk.php
+++ pages/CekSuratJalanMasuk.php
@@ -49,14 +49,14 @@ $tanggalAkhir = isset($_POST['tanggal_akhir']) ? $_POST['tanggal_akhir'] : '';
               <th rowspan="2" valign="middle" style="text-align: center">No</th>
               <th rowspan="2" valign="middle" style="text-align: center">TGLBuat KNT</th>
               <th rowspan="2" valign="middle" style="text-align: center">No BON</th>
+              <th rowspan="2" valign="middle" style="text-align: center">Project Code</th>
               <th rowspan="2" valign="middle" style="text-align: center">Jenis Benang</th>
               <th rowspan="2" valign="middle" style="text-align: center">No. Hanger</th>
               <th rowspan="2" valign="middle" style="text-align: center">Lot</th>
-              <th rowspan="2" valign="middle" style="text-align: center">Elements</th>
-              <th height="18" valign="middle" style="text-align: center">KNT</th>
+              <th height="16" valign="middle" style="text-align: center">KNT</th>
               <th valign="middle" style="text-align: center">GD Transit</th>
-              <th colspan="3" valign="middle" style="text-align: center">Masuk GKG</th>
-              <th colspan="2" valign="middle" style="text-align: center">Balance</th>
+              <th colspan="2" valign="middle" style="text-align: center">Masuk GKG</th>
+              <th colspan="1" valign="middle" style="text-align: center">Balance</th>
               <th rowspan="2" valign="middle" style="text-align: center">Status</th>
             </tr>
             <tr>
@@ -64,9 +64,7 @@ $tanggalAkhir = isset($_POST['tanggal_akhir']) ? $_POST['tanggal_akhir'] : '';
               <th valign="middle" style="text-align: center">Berat/Kg</th>
               <th valign="middle" style="text-align: center">Berat/Kg</th>
               <th valign="middle" style="text-align: center">TGL</th>
-              <th valign="middle" style="text-align: center">Block</th>
               <th valign="middle" style="text-align: center">Berat/Kg</th>
-              <th valign="middle" style="text-align: center">Block</th>
             </tr>
           </thead>
           <tbody>
@@ -78,6 +76,7 @@ $c = 0;
 $sqlDB21 = " SELECT
     DATA_SURAT_JALAN_MASUK.ORDERCODE,
     DATA_SURAT_JALAN_MASUK.ORDERLINE,
+    MAX(DATA_SURAT_JALAN_MASUK.PROJECTCODE) AS PROJECTCODE,
     MAX(DATA_SURAT_JALAN_MASUK.DECOSUBCODE01) AS DECOSUBCODE01,
     MAX(DATA_SURAT_JALAN_MASUK.DECOSUBCODE02) AS DECOSUBCODE02,
     MAX(DATA_SURAT_JALAN_MASUK.DECOSUBCODE03) AS DECOSUBCODE03,
@@ -97,6 +96,7 @@ FROM (
     SELECT
         STOCKTRANSACTION.ORDERCODE,
         STOCKTRANSACTION.ORDERLINE,
+        STOCKTRANSACTION.PROJECTCODE,
         STOCKTRANSACTION.DECOSUBCODE01,
         STOCKTRANSACTION.DECOSUBCODE02,
         STOCKTRANSACTION.DECOSUBCODE03,
@@ -119,6 +119,7 @@ FROM (
     GROUP BY
         STOCKTRANSACTION.ORDERCODE,
         STOCKTRANSACTION.ORDERLINE,
+        STOCKTRANSACTION.PROJECTCODE,
         STOCKTRANSACTION.DECOSUBCODE01,
         STOCKTRANSACTION.DECOSUBCODE02,
         STOCKTRANSACTION.DECOSUBCODE03,
@@ -144,40 +145,100 @@ $knitt = "";
 
 while ($rowdb21 = db2_fetch_assoc($stmt1)) {
     $bon = $rowdb21['ORDERCODE'] . "-" . $rowdb21['ORDERLINE'];
-    $sqlDB22 = "SELECT STOCKTRANSACTION.TRANSACTIONDATE,STOCKTRANSACTION.LOGICALWAREHOUSECODE,
-		STOCKTRANSACTION.ITEMELEMENTCODE,STOCKTRANSACTION.BASESECONDARYQUANTITY,
-		STOCKTRANSACTION.BASEPRIMARYQUANTITY,STOCKTRANSACTION.WHSLOCATIONWAREHOUSEZONECODE,
-		STOCKTRANSACTION.WAREHOUSELOCATIONCODE,STOCKTRANSACTION.LOTCODE
-		FROM DB2ADMIN.STOCKTRANSACTION STOCKTRANSACTION
-		WHERE STOCKTRANSACTION.LOGICALWAREHOUSECODE='M021' AND STOCKTRANSACTION.TOKENCODE ='RECEIPT' AND
-		STOCKTRANSACTION.TEMPLATECODE ='204' AND
-		STOCKTRANSACTION.ITEMELEMENTCODE='$rowdb21[ITEMELEMENTCODE]'";
+    $NoBon = $rowdb21['ORDERCODE'];
+    $NoLine = $rowdb21['ORDERLINE'];
+
+    $itemElementCodes = [];
+
+    $sqlDetail = " SELECT
+      STOCKTRANSACTION.ORDERCODE,
+      STOCKTRANSACTION.ORDERLINE,
+      STOCKTRANSACTION.DECOSUBCODE01,
+      STOCKTRANSACTION.DECOSUBCODE02,
+      STOCKTRANSACTION.DECOSUBCODE03,
+      STOCKTRANSACTION.DECOSUBCODE04,
+      STOCKTRANSACTION.LOTCODE,
+      STOCKTRANSACTION.TRANSACTIONDATE,
+      SUM(STOCKTRANSACTION.BASEPRIMARYQUANTITY) AS QTY_KG,
+      SUM(STOCKTRANSACTION.BASESECONDARYQUANTITY) AS QTY_YD,
+      STOCKTRANSACTION.ITEMELEMENTCODE,
+      COUNT(STOCKTRANSACTION.ITEMELEMENTCODE) AS ROL,
+      STOCKTRANSACTION.CREATIONUSER,
+      STOCKTRANSACTION.LOGICALWAREHOUSECODE,
+      STOCKTRANSACTION.WHSLOCATIONWAREHOUSEZONECODE,
+      STOCKTRANSACTION.WAREHOUSELOCATIONCODE,
+      FULLITEMKEYDECODER.SUMMARIZEDDESCRIPTION
+      FROM DB2ADMIN.STOCKTRANSACTION STOCKTRANSACTION
+      LEFT OUTER JOIN DB2ADMIN.FULLITEMKEYDECODER FULLITEMKEYDECODER ON
+      STOCKTRANSACTION.FULLITEMIDENTIFIER = FULLITEMKEYDECODER.IDENTIFIER
+      WHERE STOCKTRANSACTION.LOGICALWAREHOUSECODE ='M502' AND
+      STOCKTRANSACTION.ORDERCODE='$NoBon' AND STOCKTRANSACTION.ORDERLINE='$NoLine'
+      GROUP BY
+      STOCKTRANSACTION.ORDERCODE,
+      STOCKTRANSACTION.ORDERLINE,
+      STOCKTRANSACTION.DECOSUBCODE01,
+      STOCKTRANSACTION.DECOSUBCODE02,
+      STOCKTRANSACTION.DECOSUBCODE03,
+      STOCKTRANSACTION.DECOSUBCODE04,
+      STOCKTRANSACTION.LOTCODE,
+      STOCKTRANSACTION.TRANSACTIONDATE,
+      STOCKTRANSACTION.ITEMELEMENTCODE,
+      STOCKTRANSACTION.CREATIONUSER,
+      STOCKTRANSACTION.LOGICALWAREHOUSECODE,
+      STOCKTRANSACTION.WHSLOCATIONWAREHOUSEZONECODE,
+      STOCKTRANSACTION.WAREHOUSELOCATIONCODE,
+      FULLITEMKEYDECODER.SUMMARIZEDDESCRIPTION
+      ";
+
+    $stmtDetail = db2_exec($conn1, $sqlDetail, ['cursor' => DB2_SCROLLABLE]);
+
+    if ($stmtDetail) {
+        while ($row = db2_fetch_assoc($stmtDetail)) {
+            $itemElementCodes[] = $row['ITEMELEMENTCODE'];
+        }
+        $itemElementCodesList = "'" . implode("','", $itemElementCodes) . "'";
+    }
+
+    $sqlDB22 = "SELECT
+    MAX(STOCKTRANSACTION.TRANSACTIONDATE) AS TRANSACTIONDATE,
+    SUM(STOCKTRANSACTION.BASESECONDARYQUANTITY) AS BASESECONDARYQUANTITY,
+    SUM(STOCKTRANSACTION.BASEPRIMARYQUANTITY) AS BASEPRIMARYQUANTITY
+    FROM DB2ADMIN.STOCKTRANSACTION STOCKTRANSACTION
+    WHERE
+    STOCKTRANSACTION.LOGICALWAREHOUSECODE = 'M021'
+    AND STOCKTRANSACTION.TOKENCODE = 'RECEIPT'
+    AND STOCKTRANSACTION.TEMPLATECODE = '204'
+    AND STOCKTRANSACTION.ITEMELEMENTCODE IN ($itemElementCodesList)";
+
     $stmt2 = db2_exec($conn1, $sqlDB22, array('cursor' => DB2_SCROLLABLE));
     $rD = db2_fetch_assoc($stmt2);
-    $sqlDB23 = "SELECT BASEPRIMARYQUANTITYUNIT,BASESECONDARYQUANTITYUNIT,WHSLOCATIONWAREHOUSEZONECODE,WAREHOUSELOCATIONCODE
+
+    $sqlDB23 = "SELECT
+    SUM(BASEPRIMARYQUANTITYUNIT) AS BASEPRIMARYQUANTITYUNIT,
+    SUM(BASESECONDARYQUANTITYUNIT) AS BASESECONDARYQUANTITYUNIT
 		FROM DB2ADMIN.BALANCE BALANCE
-		WHERE BALANCE.LOGICALWAREHOUSECODE='M021' AND
-		BALANCE.ELEMENTSCODE='$rowdb21[ITEMELEMENTCODE]'";
+		WHERE BALANCE.LOGICALWAREHOUSECODE='M021'
+    AND BALANCE.ELEMENTSCODE IN ($itemElementCodesList)";
+
     $stmt3 = db2_exec($conn1, $sqlDB23, array('cursor' => DB2_SCROLLABLE));
     $rD1 = db2_fetch_assoc($stmt3);
+
     $sqlDB24 = "SELECT
-	SUM(BASEPRIMARYQUANTITYUNIT) AS BASEPRIMARYQUANTITYUNIT,
-	SUM(BASESECONDARYQUANTITYUNIT) AS BASESECONDARYQUANTITYUNIT,
-	WHSLOCATIONWAREHOUSEZONECODE,
-	WAREHOUSELOCATIONCODE,
-	COUNT(ELEMENTSCODE) AS ROL
-FROM
-	DB2ADMIN.BALANCE BALANCE
-WHERE
-	BALANCE.LOGICALWAREHOUSECODE = 'TR11'
-	AND
-		BALANCE.ELEMENTSCODE = '$rowdb21[ITEMELEMENTCODE]'
-GROUP BY
-	WHSLOCATIONWAREHOUSEZONECODE,
-	WAREHOUSELOCATIONCODE";
+    SUM(BASEPRIMARYQUANTITYUNIT) AS BASEPRIMARYQUANTITYUNIT,
+    SUM(BASESECONDARYQUANTITYUNIT) AS BASESECONDARYQUANTITYUNIT,
+    COUNT(ELEMENTSCODE) AS ROL
+    FROM
+    DB2ADMIN.BALANCE BALANCE
+    WHERE
+    BALANCE.LOGICALWAREHOUSECODE = 'TR11'
+    AND
+    BALANCE.ELEMENTSCODE IN ($itemElementCodesList)";
+
     $stmt4 = db2_exec($conn1, $sqlDB24, array('cursor' => DB2_SCROLLABLE));
     $rD2 = db2_fetch_assoc($stmt4);
+
     $stts = "";
+
     if ($rowdb21['QTY_KG'] == $rD['BASEPRIMARYQUANTITY']) {
         $stts = "<small class='badge badge-success'> OK</small>";
     } elseif (($rowdb21['QTY_KG'] > $rD['BASEPRIMARYQUANTITY'] and $rD1['BASEPRIMARYQUANTITYUNIT'] > 0) or ($rD2['BASEPRIMARYQUANTITYUNIT'] > 0 and $rowdb21['QTY_KG'] > $rD2['BASEPRIMARYQUANTITYUNIT'])) {
@@ -187,23 +248,20 @@ GROUP BY
     } else if ($rowdb21['QTY_KG'] > 0 and $rD['BASEPRIMARYQUANTITY'] > 0 and $rD1['BASEPRIMARYQUANTITYUNIT'] == "0") {
         $stts = "<small class='badge badge-info'><i class='far fa-clock blink_me'></i> Sudah Pakai</small>";
     }
-
     ?>
               <tr>
                 <td style="text-align: center"><?php echo $no; ?></td>
                 <td style="text-align: center"><?php echo $rowdb21['TRANSACTIONDATE']; ?></td>
                 <td style="text-align: center"><?php echo $bon; ?></td>
+                <td style="text-align: center"><?php echo $rowdb21['PROJECTCODE']; ?></td>
                 <td style="text-align: left"><?php echo $rowdb21['SUMMARIZEDDESCRIPTION']; ?></td>
                 <td style="text-align: center"><?php echo trim($rowdb21['DECOSUBCODE02']) . trim($rowdb21['DECOSUBCODE03']) . " " . $rowdb21['DECOSUBCODE04']; ?></td>
                 <td style="text-align: center"><?php echo $rowdb21['LOTCODE']; ?></td>
-                <td style="text-align: right"><?php echo $rowdb21['ITEMELEMENTCODE']; ?></td>
                 <td style="text-align: right"><?php echo number_format(round($rowdb21['QTY_KG'], 2), 2); ?></td>
                 <td style="text-align: right"><?php echo number_format(round($rD2['BASEPRIMARYQUANTITYUNIT'], 2), 2); ?></td>
                 <td style="text-align: right"><?php echo number_format(round($rD['BASEPRIMARYQUANTITY'], 2), 2); ?></td>
                 <td><?php echo $rD['TRANSACTIONDATE']; ?></td>
-                <td><?php echo $rD['WHSLOCATIONWAREHOUSEZONECODE'] . "-" . $rD['WAREHOUSELOCATIONCODE']; ?></td>
                 <td style="text-align: right"><?php echo number_format(round($rD1['BASEPRIMARYQUANTITYUNIT'], 2), 2); ?></td>
-                <td><?php echo $rD1['WHSLOCATIONWAREHOUSEZONECODE'] . "-" . $rD1['WAREHOUSELOCATIONCODE']; ?></td>
                 <td><?php echo $stts; ?></td>
               </tr>
 
@@ -232,10 +290,8 @@ $no++;
               <td style="text-align: right"><strong><?php echo $TkntKGS; ?></strong></td>
               <td style="text-align: right"><strong><?php echo $TtrnKGS; ?></strong></td>
               <td style="text-align: right"><strong><?php echo $TgkgKGS; ?></strong></td>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
+              <td style="text-align: center">&nbsp;</td>
               <td style="text-align: right"><strong><?php echo $TblcKGS; ?></strong></td>
-              <td>&nbsp;</td>
               <td>&nbsp;</td>
             </tr>
           </tfoot>

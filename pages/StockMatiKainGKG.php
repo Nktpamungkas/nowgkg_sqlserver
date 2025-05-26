d10c3ea498280cf5926289f977d198686bf34ce2
Asep Rifki <rifkipauji993@gmail.com>
2025-05-26 16:00:58 +0700

    fix query dan perhitungan di total kain mati

--- pages/StockMatiKainGKG.php
+++ pages/StockMatiKainGKG.php
@@ -19,7 +19,7 @@ $Awal  = isset($_POST['tgl_awal']) ? $_POST['tgl_awal'] : '';
         </div>
       </div>
       <!-- /.card-header -->
-      <!-- <div class="card-body">
+      <div class="card-body">
         <div class="form-group row">
           <label for="tgl_awal" class="col-md-1">Tanggal</label>
           <div class="col-md-2">
@@ -34,25 +34,28 @@ $Awal  = isset($_POST['tgl_awal']) ? $_POST['tgl_awal'] : '';
           </div>
         </div>
         <button class="btn btn-info" type="submit">Cari Data</button>
-      </div> -->
+      </div>
       <!-- /.card-body -->
     </div>
   </form>
+  <?php if ($Awal != "") { ?>
     <div class="card card-pink">
       <div class="card-header">
         <h3 class="card-title">Stock</h3>
       </div>
       <!-- /.card-header -->
-      <div class="card-body">
-        <table id="example1" class="table table-sm table-bordered table-striped" style="font-size:13px;">
+      <div class="card-body table-responsive">
+        <table id="example6" width="100%" class="table table-sm table-bordered table-hover tree" style="font-size:13px;">
           <thead>
             <tr>
               <th rowspan="2" style="text-align: center">Langganan</th>              
               <th colspan="4" style="text-align: center">Jenis Benang</th>
-              <th rowspan="2" style="text-align: center">Tipe</th>
+              <th rowspan="2" style="text-align: center">Jenis Kain</th>
               <th colspan="2" style="text-align: center">Ukuran Jadi</th>
+              <th rowspan="2" style="text-align: center">Knitt</th>
               <th rowspan="2" style="text-align: center">Knit Order</th>
               <th colspan="2" style="text-align: center">Stock</th>
+              <th rowspan="2" style="text-align: center">Keterangan</th>
             </tr>
             <tr>
               <th style="text-align: center">1</th>
@@ -72,37 +75,50 @@ $Awal  = isset($_POST['tgl_awal']) ? $_POST['tgl_awal'] : '';
             $QTPR = 0;
             
             // Ini query utama, udah disesuain supaya sama, tapi coba cek lagi 
-            $sql = sqlsrv_query($con, "SELECT *
-                                                  FROM (
-                                                      SELECT
-                                                          MIN(id) AS id,
-                                                          TRIM(proj_awal) AS proj_awal,
-                                                          TRIM(no_item) AS no_item,
-                                                          MAX(langganan) AS langganan,
-                                                          MAX(buyer) AS buyer,
-                                                          MAX(lot) AS lot,
-                                                          MAX(tipe) AS tipe,
-                                                          MAX(benang_1) AS benang_1,
-                                                          MAX(benang_2) AS benang_2,
-                                                          MAX(benang_3) AS benang_3,
-                                                          MAX(benang_4) AS benang_4,
-                                                          SUM(weight) AS kgs,
-                                                          SUM(rol) AS roll,
-                                                          CASE 
-                                                              WHEN CHARINDEX('/', REVERSE(TRIM(proj_awal))) > 0 
-                                                              THEN LEFT(REVERSE(LEFT(REVERSE(TRIM(proj_awal)), CHARINDEX('/', REVERSE(TRIM(proj_awal))) - 1)), 2)
-                                                              ELSE NULL
-                                                          END AS tahun_mati
-                                                      FROM
-                                                          dbnow_gkg.tblopname
-                                                      WHERE
-                                                       proj_awal LIKE '%/%'
-                                                      GROUP BY
-                                                          TRIM(proj_awal),
-                                                          TRIM(no_item)
-                                                  ) AS derived
-                                                  WHERE tahun_mati IN ('19', '20', '21')
-                                                  ORDER BY id ASC");
+            $sql = sqlsrv_query($con, "WITH RankedData AS (
+                                                                      SELECT
+                                                                        id,
+                                                                        TRIM(proj_awal) AS proj_awal,
+                                                                        TRIM(no_item) AS no_item,
+                                                                        langganan,
+                                                                        buyer,
+                                                                        lot,
+                                                                        tipe,
+                                                                        benang_1,
+                                                                        benang_2,
+                                                                        benang_3,
+                                                                        benang_4,
+                                                                        weight AS kgs,
+                                                                        rol AS roll,
+                                                                        tgl_tutup,
+                                                                        CASE 
+                                                                            WHEN CHARINDEX('/', REVERSE(TRIM(proj_awal))) > 0 
+                                                                            THEN '20' + LEFT(
+                                                                                REVERSE(
+                                                                                    LEFT(
+                                                                                        REVERSE(TRIM(proj_awal)),
+                                                                                        CHARINDEX('/', REVERSE(TRIM(proj_awal))) - 1
+                                                                                    )
+                                                                                ),
+                                                                                2
+                                                                            )
+                                                                            ELSE NULL
+                                                                        END AS tahun_mati,
+                                                                        ROW_NUMBER() OVER (
+                                                                          PARTITION BY TRIM(proj_awal), TRIM(no_item)
+                                                                          ORDER BY tgl_tutup DESC
+                                                                        ) AS rn
+                                                                      FROM dbnow_gkg.tblopname
+                                                                      WHERE 
+                                                                        proj_awal LIKE '%/%'
+                                                                        and  tgl_tutup = '$Awal'
+                                                                    --    AND proj_awal = '2023562/XII/21'
+                                                                    )
+                                                                    SELECT *
+                                                                    FROM RankedData
+                                                                    WHERE rn = 1
+                                                                    and tahun_mati IN ('2019', '2020', '2021')
+                                                                    ORDER BY tahun_mati ASC");
             // End Query utama
 
             while ($r = sqlsrv_fetch_array($sql)) {
@@ -111,44 +127,44 @@ $Awal  = isset($_POST['tgl_awal']) ? $_POST['tgl_awal'] : '';
                                           ");		  
                                           $r1 = mysqli_fetch_array($sql1);
 
-              // Query penyebab lemot
-              $sqlDB210 = "SELECT SUM(a.BASEPRIMARYQUANTITY) AS BASEPRIMARYQUANTITY, SUM(a3.VALUEDECIMAL) AS QTYSALIN  FROM ITXVIEWHEADERKNTORDER a
-                LEFT OUTER JOIN PRODUCTIONDEMAND p ON p.CODE =a.PRODUCTIONDEMANDCODE
-                LEFT OUTER JOIN ADSTORAGE a2 ON p.ABSUNIQUEID =a2.UNIQUEID AND a2.NAMENAME ='StatusRMP'
-                LEFT OUTER JOIN ADSTORAGE a3 ON p.ABSUNIQUEID =a3.UNIQUEID AND a3.NAMENAME ='QtySalin'
-                WHERE a.ITEMTYPEAFICODE ='KGF' AND (a.PROJECTCODE ='$r[proj_awal]' OR a.ORIGDLVSALORDLINESALORDERCODE ='$r[proj_awal]') AND
-                (a.PROGRESSSTATUS='2' OR a.PROGRESSSTATUS='6') AND (NOT a2.VALUESTRING ='3' OR a2.VALUESTRING IS NULL) AND 
-                CONCAT(TRIM(a.SUBCODE02),CONCAT(TRIM(a.SUBCODE03),CONCAT(' ',TRIM(a.SUBCODE04))))='$r[no_item]'
-                GROUP BY a.SUBCODE02,a.SUBCODE03,a.SUBCODE04";
-              // End Query
+              // // Query penyebab lemot
+              // $sqlDB210 = "SELECT SUM(a.BASEPRIMARYQUANTITY) AS BASEPRIMARYQUANTITY, SUM(a3.VALUEDECIMAL) AS QTYSALIN  FROM ITXVIEWHEADERKNTORDER a
+              //   LEFT OUTER JOIN PRODUCTIONDEMAND p ON p.CODE =a.PRODUCTIONDEMANDCODE
+              //   LEFT OUTER JOIN ADSTORAGE a2 ON p.ABSUNIQUEID =a2.UNIQUEID AND a2.NAMENAME ='StatusRMP'
+              //   LEFT OUTER JOIN ADSTORAGE a3 ON p.ABSUNIQUEID =a3.UNIQUEID AND a3.NAMENAME ='QtySalin'
+              //   WHERE a.ITEMTYPEAFICODE ='KGF' AND (a.PROJECTCODE ='$r[proj_awal]' OR a.ORIGDLVSALORDLINESALORDERCODE ='$r[proj_awal]') AND
+              //   (a.PROGRESSSTATUS='2' OR a.PROGRESSSTATUS='6') AND (NOT a2.VALUESTRING ='3' OR a2.VALUESTRING IS NULL) AND 
+              //   CONCAT(TRIM(a.SUBCODE02),CONCAT(TRIM(a.SUBCODE03),CONCAT(' ',TRIM(a.SUBCODE04))))='$r[no_item]'
+              //   GROUP BY a.SUBCODE02,a.SUBCODE03,a.SUBCODE04";
+              // // End Query
 
-              $stmt10   = db2_exec($conn1, $sqlDB210, array('cursor' => DB2_SCROLLABLE));
-              $rowdb210 = db2_fetch_assoc($stmt10);
+              // $stmt10   = db2_exec($conn1, $sqlDB210, array('cursor' => DB2_SCROLLABLE));
+              // $rowdb210 = db2_fetch_assoc($stmt10);
               
-              // Penyebab lemotnya ini karena di tiap rownya harus ngebaca ulang ini
-              // Mungkin solusinya bisa buat query kepisah untuk ambil value qtpr
-              $QTPR = $rowdb210['BASEPRIMARYQUANTITY'] - $rowdb210['QTYSALIN'];
-              // End Penyebab Lemot
+              // // Penyebab lemotnya ini karena di tiap rownya harus ngebaca ulang ini
+              // // Mungkin solusinya bisa buat query kepisah untuk ambil value qtpr
+              // $QTPR = $rowdb210['BASEPRIMARYQUANTITY'] - $rowdb210['QTYSALIN'];
+              // // End Penyebab Lemot
 
-              $sqlDB211 = "SELECT SUM(STOCKTRANSACTION.BASEPRIMARYQUANTITY) AS QTY_KG,COUNT(STOCKTRANSACTION.BASEPRIMARYQUANTITY) AS QTY_ROL  
-                                FROM DB2ADMIN.STOCKTRANSACTION STOCKTRANSACTION LEFT OUTER JOIN
-                                DB2ADMIN.ITXVIEWLAPMASUKGREIGE ITXVIEWLAPMASUKGREIGE ON ITXVIEWLAPMASUKGREIGE.PROVISIONALCODE  = STOCKTRANSACTION.ORDERCODE
-                                AND ITXVIEWLAPMASUKGREIGE.ORDERLINE  = STOCKTRANSACTION.ORDERLINE
-                                AND ITXVIEWLAPMASUKGREIGE.PROVISIONALCOUNTERCODE  = STOCKTRANSACTION.ORDERCOUNTERCODE  
-                                AND ITXVIEWLAPMASUKGREIGE.ITEMTYPEAFICODE = STOCKTRANSACTION.ITEMTYPECODE 
-                                AND ITXVIEWLAPMASUKGREIGE.SUBCODE01= STOCKTRANSACTION.DECOSUBCODE01
-                                AND ITXVIEWLAPMASUKGREIGE.SUBCODE02= STOCKTRANSACTION.DECOSUBCODE02
-                                AND ITXVIEWLAPMASUKGREIGE.SUBCODE03= STOCKTRANSACTION.DECOSUBCODE03
-                                AND ITXVIEWLAPMASUKGREIGE.SUBCODE04= STOCKTRANSACTION.DECOSUBCODE04
-                                WHERE STOCKTRANSACTION.PROJECTCODE='$r[proj_awal]' AND 
-                                CONCAT(TRIM(STOCKTRANSACTION.DECOSUBCODE02),CONCAT(TRIM(STOCKTRANSACTION.DECOSUBCODE03),CONCAT(' ',TRIM(STOCKTRANSACTION.DECOSUBCODE04))))='$r[no_item]' and 
-                                STOCKTRANSACTION.LOGICALWAREHOUSECODE ='M021' 
-                                AND NOT ITXVIEWLAPMASUKGREIGE.ORDERLINE IS NULL
-                                GROUP BY 
-                                STOCKTRANSACTION.DECOSUBCODE02,STOCKTRANSACTION.DECOSUBCODE03,STOCKTRANSACTION.DECOSUBCODE04,STOCKTRANSACTION.LOGICALWAREHOUSECODE
-                                ";
-              $stmt11   = db2_exec($conn1, $sqlDB211, array('cursor' => DB2_SCROLLABLE));
-              $rowdb211 = db2_fetch_assoc($stmt11);
+              // $sqlDB211 = "SELECT SUM(STOCKTRANSACTION.BASEPRIMARYQUANTITY) AS QTY_KG,COUNT(STOCKTRANSACTION.BASEPRIMARYQUANTITY) AS QTY_ROL  
+              //                   FROM DB2ADMIN.STOCKTRANSACTION STOCKTRANSACTION LEFT OUTER JOIN
+              //                   DB2ADMIN.ITXVIEWLAPMASUKGREIGE ITXVIEWLAPMASUKGREIGE ON ITXVIEWLAPMASUKGREIGE.PROVISIONALCODE  = STOCKTRANSACTION.ORDERCODE
+              //                   AND ITXVIEWLAPMASUKGREIGE.ORDERLINE  = STOCKTRANSACTION.ORDERLINE
+              //                   AND ITXVIEWLAPMASUKGREIGE.PROVISIONALCOUNTERCODE  = STOCKTRANSACTION.ORDERCOUNTERCODE  
+              //                   AND ITXVIEWLAPMASUKGREIGE.ITEMTYPEAFICODE = STOCKTRANSACTION.ITEMTYPECODE 
+              //                   AND ITXVIEWLAPMASUKGREIGE.SUBCODE01= STOCKTRANSACTION.DECOSUBCODE01
+              //                   AND ITXVIEWLAPMASUKGREIGE.SUBCODE02= STOCKTRANSACTION.DECOSUBCODE02
+              //                   AND ITXVIEWLAPMASUKGREIGE.SUBCODE03= STOCKTRANSACTION.DECOSUBCODE03
+              //                   AND ITXVIEWLAPMASUKGREIGE.SUBCODE04= STOCKTRANSACTION.DECOSUBCODE04
+              //                   WHERE STOCKTRANSACTION.PROJECTCODE='$r[proj_awal]' AND 
+              //                   CONCAT(TRIM(STOCKTRANSACTION.DECOSUBCODE02),CONCAT(TRIM(STOCKTRANSACTION.DECOSUBCODE03),CONCAT(' ',TRIM(STOCKTRANSACTION.DECOSUBCODE04))))='$r[no_item]' and 
+              //                   STOCKTRANSACTION.LOGICALWAREHOUSECODE ='M021' 
+              //                   AND NOT ITXVIEWLAPMASUKGREIGE.ORDERLINE IS NULL
+              //                   GROUP BY 
+              //                   STOCKTRANSACTION.DECOSUBCODE02,STOCKTRANSACTION.DECOSUBCODE03,STOCKTRANSACTION.DECOSUBCODE04,STOCKTRANSACTION.LOGICALWAREHOUSECODE
+              //                   ";
+              // $stmt11   = db2_exec($conn1, $sqlDB211, array('cursor' => DB2_SCROLLABLE));
+              // $rowdb211 = db2_fetch_assoc($stmt11);
 
               $sqlDB28 = " SELECT a.VALUEDECIMAL  FROM PRODUCT p 
 LEFT OUTER JOIN ADSTORAGE a  ON a.UNIQUEID = p.ABSUNIQUEID 
@@ -171,12 +187,14 @@ p.ITEMTYPECODE ='KFF'  ";
                 <td style="text-align: left"><?php echo $r['benang_2']; ?></td>
                 <td style="text-align: left"><?php echo $r['benang_3']; ?></td>
                 <td style="text-align: left"><?php echo $r['benang_4']; ?></td>
-                <td style="text-align: center"><?php echo $r['tipe']; ?></td>
+                <td style="text-align: center"><?php echo $r['no_item']; ?></td>
                 <td style="text-align: center"><?php echo round($rowdb28['VALUEDECIMAL']); ?></td>
                 <td style="text-align: center"><?php echo round($rowdb29['VALUEDECIMAL']); ?></td>  
+                <td style="text-align: center"></td>
                 <td style="text-align: center"><?php echo $r['proj_awal']; ?></td>
                 <td style="text-align: center"><?php echo $r['roll']; ?></td>
                 <td style="text-align: right"><?php echo $r['kgs']; ?></td>
+                <td style="text-align: center"><?php echo $r['tahun_mati']; ?></td>
                 
               </tr>
             <?php
@@ -193,15 +211,18 @@ p.ITEMTYPECODE ='KFF'  ";
               <td style="text-align: center">&nbsp;</td>
               <td style="text-align: center">&nbsp;</td>
               <td style="text-align: center">&nbsp;</td>
+              <td style="text-align: center">&nbsp;</td>
               <td colspan="4" style="text-align: right"><strong>TOTAL</strong></td>
               <td style="text-align: center"><span style="text-align: right"><strong><?php echo $totrol; ?></strong></span></td>
               <td style="text-align: right"><strong><?php echo number_format(round($totkg, 3), 3); ?></strong></td> 
+              <td style="text-align: center">&nbsp;</td>
             </tr>
           </tfoot>
         </table>
       </div>
       <!-- /.card-body -->
     </div>
+    <?php } ?>
 </div><!-- /.container-fluid -->
 <!-- /.content -->
 <script>
